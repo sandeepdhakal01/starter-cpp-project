517e520470d0d4d39a612f72ad046e3be5ad2d75
Sandeep Dhakal <sandeepdhakal789@gmail.com>
2026-05-17 05:45:53 +0000

    Weekend/weekday checker

--- src/video2.cpp
+++ src/video2.cpp
@@ -1,22 +1,14 @@
 #include <iostream>
-#include <cctype>
+#include <iomanip>
 using namespace std;
 
 int main(){
-  string day;
-  cout << "Enter a day: ";
-  cin >> day;
-
-  for(int i = 0; i < day.length(); i++){
-    day[i] = tolower(day[i]);
-  }
-
-  if(day == "fri"){
-    cout << "It's Friday!" << endl;
-  } else if(day == "sun" || day == "sat"){
-    cout << "It's a weekend." << endl;
-  } else {
-    cout << "It's a weekday." << endl;
+  cout << setw(6) << "Odd" << setw(8) << "Even" << endl;
+  
+  int i = 1;
+  while(i <= 19){
+    cout << setw(6) << i << setw(8) << (i + 1) << endl;
+    i += 2;
   }
 
   return 0;
